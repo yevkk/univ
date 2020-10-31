8a719c51f1624ded83f55bf18dbd7122192b3f2e
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2020-10-31 16:43:51 +0200

    edit: modified prompt

--- operating_systems/lab1/src/manager.cpp
+++ operating_systems/lab1/src/manager.cpp
@@ -145,16 +145,17 @@ namespace spos::lab1 {
 
     Manager::RunExitCode Manager::run() {
         std::atomic<bool> done = false;
+        auto start_ts = system_clock::now();
 
         auto keyboard_listener = std::async(std::launch::async,
-                              [&done]() {
-                                  while (true) {
-                                      std::this_thread::sleep_for(50ms);
-                                      if (done || (GetKeyState(VK_ESCAPE) & 0x8000)) {
-                                          break;
-                                      }
-                                  }
-                              }
+                                            [&done]() {
+                                                while (true) {
+                                                    std::this_thread::sleep_for(50ms);
+                                                    if (done || (GetKeyState(VK_ESCAPE) & 0x8000)) {
+                                                        break;
+                                                    }
+                                                }
+                                            }
         );
 
         WSADATA wsa_data;
@@ -201,6 +202,7 @@ namespace spos::lab1 {
         }
 
         _sub_results = decltype(_sub_results)(func_futures.size(), std::nullopt);
+        bool prompt_enabled = true;
         auto next_prompt_ts = system_clock::now() + PROMPT_PERIOD;
         while (!func_futures.empty()) {
             if (keyboard_listener.wait_for(0s) == std::future_status::ready) {
@@ -209,17 +211,25 @@ namespace spos::lab1 {
                 return TERMINATED;
             }
 
-            if (system_clock::now() > next_prompt_ts) {
-                std::cout << "Enter 0 to terminate: ";
-                int input;
-                std::cin >> input;
-                if (input == 0) {
-                    done = true;
-                    _terminateUnfinished();
-                    WSACleanup();
-                    return TERMINATED;
+            if (system_clock::now() > next_prompt_ts && prompt_enabled) {
+                std::cout << "[INFO] Time elapsed: " << duration_cast<seconds>(system_clock::now() - start_ts).count()
+                          << "s\n";
+                std::cout << "Choose Option:\n \ta) continue\n \tb) continue without prompt\n \tc) stop\n";
+                char input = ' ';
+                while (input != 'a' && input != 'b' && input != 'c') {
+                    std::cin >> input;
+                    if (input == 'a') {
+                        std::cout << "[INFO] Continued\n";
+                    } else if (input == 'b') {
+                        std::cout << "[INFO] Prompt disabled\n";
+                        prompt_enabled = false;
+                    } else if (input == 'c') {
+                        done = true;
+                        _terminateUnfinished();
+                        WSACleanup();
+                        return TERMINATED;
+                    }
                 }
-                next_prompt_ts += PROMPT_PERIOD;
                 next_prompt_ts = system_clock::now() + PROMPT_PERIOD;
             }
 
