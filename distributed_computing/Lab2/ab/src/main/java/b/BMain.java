52b8f2fd8f9a73a7a7c55b31407b34da7c764de0
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2021-02-21 18:10:20 +0200

    Add burglars

--- distributed_computing/Lab2/ab/src/main/java/b/BMain.java
+++ distributed_computing/Lab2/ab/src/main/java/b/BMain.java
@@ -1,4 +1,77 @@
 package b;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class BMain {
+    public static void main(String[] args) {
+        Queue<Item> storage = new LinkedList<Item>();
+        //Todo: fill storage
+        var midpoint = new PCItemQueue();
+        var truck = new PCItemQueue();
+        var sync = new AtomicBoolean();
+
+        var Ivanov = new Burglar(midpoint, sync) {
+            private Queue<Item> storage;
+
+            public void setStorage(Queue<Item> storage) {
+                this.storage = storage;
+            }
+
+            @Override
+            public void action() {
+                var item = this.storage.poll();
+                if (item != null) {
+                    this.queue.put(item);
+                } else {
+                    this.sync.set(false);
+                }
+            }
+        };
+        Ivanov.setStorage(storage);
+
+        var Petrov = new Burglar(midpoint, sync) {
+            private PCItemQueue additionalQueue;
+
+            public void setAdditionalQueue(PCItemQueue additionalQueue) {
+                this.additionalQueue = additionalQueue;
+            }
+
+            @Override
+            public void action() {
+                this.additionalQueue.put(this.queue.get());
+            }
+        };
+        Petrov.setAdditionalQueue(truck);
+
+        var Nechiporuk = new Burglar(truck, sync) {
+            private int sum = 0;
+
+            @Override
+            public void action() {
+                sum += this.queue.get().getCost();
+            }
+
+            public int getSum() {
+                return this.sum;
+            }
+        };
+
+
+        try {
+            Ivanov.getThread().join();
+            Petrov.getThread().join();
+            Nechiporuk.getThread().join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(Nechiporuk.getSum());
+    }
+
+
+
+
+
 }
---
+++ distributed_computing/Lab2/ab/src/main/java/b/Burglar.java
@@ -0,0 +1,28 @@
+package b;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public abstract class Burglar implements Runnable{
+    protected final PCItemQueue queue;
+    protected final AtomicBoolean sync;
+    private final Thread thread;
+
+    public Burglar(PCItemQueue queue, AtomicBoolean sync) {
+        this.queue = queue;
+        this.sync = sync;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public void run() {
+        while(sync.get()) {
+            action();
+        }
+    }
+
+    public abstract void action();
+
+    public Thread getThread() {
+        return thread;
+    }
+}
