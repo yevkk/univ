1de01d2c90a9d50f22ed0d4dc03885e33205656b
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2021-03-02 19:29:43 +0200

    Modified copied r/w lock

--- distributed_computing/Lab4/ab/src/main/java/b/RWLock.java
+++ distributed_computing/Lab4/ab/src/main/java/b/RWLock.java
@@ -5,9 +5,13 @@ public class RWLock {
     private int wCounter = 0;
     private int wRequests = 0;
 
-    public synchronized void lockRead() throws InterruptedException {
-        while (wCounter > 0 || wRequests > 0) {
-            wait();
+    public synchronized void lockRead() {
+        try {
+            while (wCounter > 0 || wRequests > 0) {
+                wait();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         rCounter++;
     }
@@ -17,17 +21,20 @@ public class RWLock {
         notifyAll();
     }
 
-    public synchronized void lockWrite() throws InterruptedException {
+    public synchronized void lockWrite() {
         wRequests++;
-
-        while (rCounter > 0 || wCounter > 0) {
-            wait();
+        try {
+            while (rCounter > 0 || wCounter > 0) {
+                wait();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         wRequests--;
         wCounter++;
     }
 
-    public synchronized void unlockWrite() throws InterruptedException {
+    public synchronized void unlockWrite() {
         wCounter--;
         notifyAll();
     }
