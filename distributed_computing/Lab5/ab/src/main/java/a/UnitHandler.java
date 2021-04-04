57ebd4d55e3028ea8c1cecb19930f9c2a961d66d
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2021-04-04 19:06:22 +0300

    Fix unit handler

--- distributed_computing/Lab5/ab/src/main/java/a/UnitHandler.java
+++ distributed_computing/Lab5/ab/src/main/java/a/UnitHandler.java
@@ -68,28 +68,48 @@ public class UnitHandler implements Runnable {
         try {
             while (!done.get()) {
                 var next = new State[current.length];
+                modified = false;
                 for (int i = 1; i < current.length - 1; i++) {
+                    next[i] = current[i];
                     if ((current[i] == State.L && current[i - 1] == State.R) || (current[i] == State.R && current[i + 1] == State.L)) {
                         next[i] = current[i].turn();
                         modified = true;
-                    } else {
-                        next[i] = current[i];
                     }
                 }
-                if (left != null && current[0] == State.L && left == State.R) {
-                    next[0] = current[0].turn();
-                    modified = true;
-                } else {
-                    next[0] = current[0];
-                }
-                if (right != null && current[current.length - 1] == State.R && right == State.L) {
-                    next[next.length - 1] = current[current.length - 1].turn();
-                    modified = true;
-                } else {
-                    next[next.length - 1] = current[current.length - 1];
-                }
-                current = next;
 
+                next[0] = current[0];
+                switch (current[0]) {
+                    case L:
+                        if (left != null && left == State.R) {
+                            next[0] = current[0].turn();
+                            modified = true;
+                        }
+                        break;
+                    case R:
+                        if (current[1] == State.L) {
+                            next[0] = current[0].turn();
+                            modified = true;
+                        }
+                        break;
+                }
+
+                next[next.length - 1] = current[current.length - 1];
+                switch (current[current.length - 1]) {
+                    case L:
+                        if (current[current.length - 2] == State.R) {
+                            next[next.length - 1] = current[current.length - 1].turn();
+                            modified = true;
+                        }
+                        break;
+                    case R:
+                        if (right != null && right == State.L) {
+                            next[next.length - 1] = current[current.length - 1].turn();
+                            modified = true;
+                        }
+                        break;
+                }
+
+                current = next;
                 barrier.await();
             }
         } catch (InterruptedException e) {
