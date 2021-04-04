d0b5a0c81cca0089d301f48a2c4a1e75535ee5dc
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2021-04-04 22:53:53 +0300

    Fix rand; Add done condition

--- distributed_computing/Lab5/с/main.go
+++ distributed_computing/Lab5/с/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func sum(arr []int) int {
@@ -13,19 +14,21 @@ func sum(arr []int) int {
 	return res
 }
 
-func arrayHandler(id int, length int, inChan chan int, outChan chan int) {
+
+var r = rand.New(rand.NewSource(time.Now().UnixNano()))
+func arrayHandler(id int, length int, inChan chan int, outChan chan int, done *bool) {
 	arr := make([]int , length)
 	for i := range arr {
-		arr[i] = rand.Intn(10)
+		arr[i] = r.Intn(10)
 	}
 
-	for {
+	for !(*done) {
 		localSum := sum(arr)
 		fmt.Printf("%v: %v\n", id, arr)
 		outChan <- localSum
 
 		globalAverage := <- inChan
-		index := rand.Intn(len(arr))
+		index := r.Intn(len(arr))
 		switch {
 		case globalAverage < localSum:
 			arr[index]--
@@ -36,13 +39,13 @@ func arrayHandler(id int, length int, inChan chan int, outChan chan int) {
 }
 
 func main() {
+	done := false
 	sendingChan := make(chan int, 3)
-
 	receivingChan := make(chan int, 3)
 
-	go arrayHandler(1, 10, sendingChan, receivingChan)
-	go arrayHandler(2, 10, sendingChan, receivingChan)
-	go arrayHandler(3, 10, sendingChan, receivingChan)
+	go arrayHandler(1, 10, sendingChan, receivingChan, &done)
+	go arrayHandler(2, 10, sendingChan, receivingChan, &done)
+	go arrayHandler(3, 10, sendingChan, receivingChan, &done)
 
 	for {
 		s1 := <- receivingChan
@@ -57,6 +60,7 @@ func main() {
 		sendingChan <- average
 		sendingChan <- average
 		if s1 == s2 && s1 == s3 {
+			done = true
 			return
 		}
 	}
