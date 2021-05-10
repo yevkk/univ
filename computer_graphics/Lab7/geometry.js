21238526b8ae93b3dda97e3386598e890f92b5df
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2021-05-10 09:30:39 +0300

    fixes

--- computer_graphics/Lab7/geometry.js
+++ computer_graphics/Lab7/geometry.js
@@ -4,14 +4,18 @@ let points = []
 let convexHull = []
 
 function addPointCH(hull, candidatePoint) {
-    drawPoint(mainCanvas.getContext('2d'), "yellow", candidatePoint)
-    ;
+    let orientation = (p, q, r) => ((q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y))
     if (hull.length < 2) {
         hull.push(candidatePoint)
         return true
     } else if (hull.length === 2) {
         //TODO:
-        hull.push(candidatePoint)
+        if (orientation(candidatePoint, hull[0], hull[1]) > 0) {
+            hull.push(candidatePoint)
+        } else {
+            hull.unshift(candidatePoint)
+        }
+
         return true
     }
 
@@ -19,9 +23,6 @@ function addPointCH(hull, candidatePoint) {
     let distSq = (p1, p2) => ((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2)
     let closestI = hull.reduce((current, point, index) => ((distSq(candidatePoint, point) < distSq(candidatePoint, hull[current])) ? index : current), 0)
     let counter = 0;
-
-    drawPoint(mainCanvas.getContext('2d'), "green", hull[closestI])
-    ;
 
     let leftI = closestI;
     while (hull[(n + leftI - 1) % n].relativeY(candidatePoint, hull[leftI]) * hull[(leftI + 1) % n].relativeY(candidatePoint, hull[leftI]) < 0) {
@@ -31,9 +32,6 @@ function addPointCH(hull, candidatePoint) {
             return false
         }
     }
-    drawPoint(mainCanvas.getContext('2d'), "blue", hull[leftI])
-    ;
-
 
     let rightI = closestI;
     while (hull[(n + rightI - 1) % n].relativeY(candidatePoint, hull[rightI]) * hull[(rightI + 1) % n].relativeY(candidatePoint, hull[rightI]) < 0 || rightI === leftI) {
@@ -46,13 +44,12 @@ function addPointCH(hull, candidatePoint) {
     drawPoint(mainCanvas.getContext('2d'), "blue", hull[rightI])
     ;
     counter--
-    let orientation = (p, q, r) => ((q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y))
     if (orientation(hull[leftI], candidatePoint, hull[rightI]) < 0) {
         [leftI, rightI] = [rightI, leftI]
-        counter = n - 2 - counter
+        if (leftI !== closestI) {
+            counter = n - 2 - counter
+        }
     }
-
-
 
     if (leftI < rightI) {
         hull.splice(leftI + 1, counter, candidatePoint)
