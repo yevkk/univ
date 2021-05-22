b7a2282959fc94a4aa92571f2419d2d738bcc119
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2021-05-23 00:43:11 +0300

    renamed again

--- distributed_computing/m2-Lab3/src/main/java/client/Client.java
+++ distributed_computing/m2-Lab3/src/main/java/client/Client.java
@@ -1,4 +1,4 @@
-package socket;
+package client;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
--- distributed_computing/m2-Lab3/src/main/java/client/ClientRunner.java
+++ distributed_computing/m2-Lab3/src/main/java/client/ClientRunner.java
@@ -1,4 +1,4 @@
-package socket;
+package client;
 
 import java.io.IOException;
 import java.util.logging.Logger;
