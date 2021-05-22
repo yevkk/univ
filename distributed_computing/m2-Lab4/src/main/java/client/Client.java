31f89bead1c41135e146b8cd6885a311a2b6eb10
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2021-05-23 02:57:14 +0300

    client

--- distributed_computing/m2-Lab4/src/main/java/client/Client.java
+++ distributed_computing/m2-Lab4/src/main/java/client/Client.java
@@ -2,56 +2,63 @@ package client;
 
 import entites.Airline;
 import entites.Flight;
+import server.IServer;
 
 import java.io.*;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 import java.util.List;
 
 
 
 public class Client {
-    public Client() {
+    private final IServer server;
 
+    public Client() throws RemoteException, NotBoundException {
+        var registry = LocateRegistry.getRegistry(8030);
+        this.server = (IServer) registry.lookup("rmi-server");
     }
 
 
-    public List<Airline> findAllAirlines() {
-
+    public List<Airline> findAllAirlines() throws RemoteException {
+        return server.findAllAirlines();
     }
 
-    public Airline findAirline(int id) {
-
+    public Airline findAirline(int id) throws RemoteException {
+        return server.findAirline(id);
     }
 
-    public boolean createAirline(Airline airline) {
-
+    public boolean createAirline(Airline airline) throws RemoteException {
+        return server.createAirline(airline);
     }
 
-    public boolean updateAirline(Airline airline) throws IOException {
-
+    public boolean updateAirline(Airline airline) throws RemoteException {
+        return server.updateAirline(airline);
     }
 
-    public boolean deleteAirline(int id) throws IOException {
-
+    public boolean deleteAirline(int id) throws RemoteException {
+        return server.deleteAirline(id);
     }
 
-    public List<Flight> findAllFlights() throws IOException {
-
+    public List<Flight> findAllFlights() throws RemoteException {
+        return server.findAllFlights();
     }
 
-    public Flight findFlight(int id) throws IOException {
-
+    public Flight findFlight(int id) throws RemoteException {
+        return server.findFlight(id);
     }
 
-    public boolean createFlight(Flight flight) {
-
+    public boolean createFlight(Flight flight) throws RemoteException {
+        return server.createFlight(flight);
     }
 
-    public boolean updateFlight(Flight flight) {
-
+    public boolean updateFlight(Flight flight) throws RemoteException {
+        return server.updateFlight(flight);
     }
 
-    public boolean deleteFlight(int id) {
-
+    public boolean deleteFlight(int id) throws RemoteException {
+        return server.deleteFlight(id);
     }
 
 }
