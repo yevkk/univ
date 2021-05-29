d04c1b8937f7011da35830ea418dcc4ec2b9fe51
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2021-05-29 20:41:18 +0300

    book stats servlets

--- java-webapp/servlet-server/src/main/java/servlet/AuthorizeHelper.java
+++ java-webapp/servlet-server/src/main/java/servlet/AuthorizeHelper.java
@@ -4,22 +4,21 @@ import connect.ConnectionPool;
 import connect.dao.UserDAO;
 import entity.misc.User;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class AuthorizeHelper {
     public static User getUser(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        var loginArr = req.getParameterValues("login");
-        var passwordArr = req.getParameterValues("password");
+        var login = req.getParameter("login");
+        var password = req.getParameter("password");
 
-        if (loginArr == null || passwordArr == null) {
+        if (login == null || password == null) {
             return null;
         }
 
         var conn = ConnectionPool.getInstance().getConnection();
-        var user = new UserDAO(conn).find(loginArr[0], passwordArr[0]);
+        var user = new UserDAO(conn).find(login, password);
         ConnectionPool.getInstance().putConnection(conn);
 
         return user;
