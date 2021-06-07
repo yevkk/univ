82b171e0e975b9be50cac97dadbeae74b0de4aa0
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2021-06-07 14:18:17 +0300

    lab2 initial commit

---
+++ cryptography/Lab2/src/main/java/RC5.java
@@ -0,0 +1,25 @@
+public class RC5 {
+    /**
+     * Odd((e - 1) * 2^w),
+     * where Odd is the nearest odd integer to the given input, e is the base of the natural logarithm
+     */
+    private static long P(int w) {
+        return switch (w) {
+            case 16 -> 0xB7E1;
+            case 32 -> 0xB7E15163;
+            default -> throw new IllegalArgumentException("supported w in {16, 32}");
+        };
+    }
+
+    /**
+     * Odd((&phi - 1) * 2^w),
+     * where Odd is the nearest odd integer to the given input, where &phi is the golden ratio
+     */
+    private static long Q(int w) {
+        return switch (w) {
+            case 16 -> 0x9E37;
+            case 32 -> 0x9E3779B9;
+            default -> throw new IllegalArgumentException("supported w in {16, 32}");
+        };
+    }
+}
