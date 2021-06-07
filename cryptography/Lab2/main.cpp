05f77c315a5d09e2736bc667fcc6dd58bc6717b0
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2021-06-07 17:38:26 +0300

    tests setup

--- cryptography/Lab2/main.cpp
+++ cryptography/Lab2/main.cpp
@@ -2,14 +2,4 @@
 #include "rc5.hpp"
 
 int main() {
-    RC5 rc5;
-    rc5.setup(std::string(16, '\0'));
-    std::uint32_t source[] = {0, 0};
-    auto res = rc5.encrypt(source);
-    std::cout << std::hex << res[0] << std::endl;
-    std::cout << std::hex << res[1] << std::endl;
-
-    auto dec = rc5.decrypt(res);
-    std::cout << std::dec << dec[0] << std::endl;
-    std::cout << std::dec << dec[1] << std::endl;
 }
