bd18db318fd8d3e36890d5254ef655bc44f81f33
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2021-07-04 18:20:21 +0300

    more entities

---
+++ java-webapp/spring/spring-server/src/main/java/entities/book/changelogs/BalanceLogRecord.java
@@ -0,0 +1,33 @@
+package entities.book.changelogs;
+
+import entities.book.Book;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "book_balance_changelog")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BalanceLogRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private LocalDateTime datetime;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @Column
+    private int amount;
+
+    @Column
+    private String comment;
+}
---
+++ java-webapp/spring/spring-server/src/main/java/entities/book/changelogs/RateLogRecord.java
@@ -0,0 +1,33 @@
+package entities.book.changelogs;
+
+import entities.book.Book;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "book_rate_changelog")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RateLogRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private LocalDateTime datetime;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @Column(name = "user_id")
+    private int userID;
+
+    @Column
+    private double contribution;
+}
