9c61b7992331c3d495edb636ae8f70964310c8c7
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2021-07-08 21:20:26 +0300

    spring boot keycloak

--- java-webapp/spring/spring-server/src/main/java/com/example/lab/controllers/BookController.java
+++ java-webapp/spring/spring-server/src/main/java/com/example/lab/controllers/BookController.java
@@ -4,29 +4,36 @@ import com.example.lab.entities.book.Book;
 import com.example.lab.services.BookStatsService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import com.example.lab.services.BookService;
+
+import javax.annotation.security.RolesAllowed;
 
 @RestController
 @RequestMapping("/books")
 @CrossOrigin(originPatterns = "*")
 @AllArgsConstructor
+@PreAuthorize("isAuthenticated()")
 public class BookController {
     private final BookService bookService;
 
     @GetMapping("")
+    @RolesAllowed({"library-user", "library-admin"})
     public ResponseEntity getAll() {
         var books = bookService.findAll();
         return ResponseEntity.ok(books);
     }
 
     @GetMapping("/{id}")
+    @RolesAllowed("library-admin")
     public ResponseEntity get(@PathVariable("id") long id) {
         var book = bookService.find(id);
         return ResponseEntity.ok(book);
     }
 
     @PostMapping("")
+    @RolesAllowed("library-admin")
     public void create(@RequestParam String name,
                        @RequestParam String author,
                        @RequestParam String lang) {
