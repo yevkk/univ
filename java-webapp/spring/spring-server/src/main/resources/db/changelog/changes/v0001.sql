35f1c4ace0237d6b9eb9a0eadd1f23637ec0ca35
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2021-07-09 02:01:19 +0300

    db modified

--- java-webapp/spring/spring-server/src/main/resources/db/changelog/changes/v0001.sql
+++ java-webapp/spring/spring-server/src/main/resources/db/changelog/changes/v0001.sql
@@ -1,12 +1,3 @@
-create table users (
-    id serial not null,
-    login text not null,
-    password text not null,
-    role text not null,
-
-    primary key (id)
-);
-
 create table book (
     id serial not null,
     name text not null,
@@ -38,14 +29,13 @@ create table book_request (
     id serial not null,
     datetime timestamp with time zone not null,
     book_id serial not null,
-    user_id serial not null,
+    user_id text not null,
     delivery_type_id serial not null,
     contact text not null,
     state text not null,
 
     primary key (id),
     foreign key (book_id) references book (id) on delete cascade,
-    foreign key (user_id) references users (id) on delete cascade,
     foreign key (delivery_type_id) references delivery_type (id) on delete cascade
 );
 
@@ -63,12 +53,11 @@ create table book_rate_changelog (
     id serial not null,
     datetime timestamp with time zone not null,
     book_id serial not null,
-    user_id serial not null,
+    user_id text not null,
     contribution double precision not null,
 
     primary key (id),
-    foreign key (book_id) references book (id) on delete cascade,
-    foreign key (user_id) references users (id) on delete cascade
+    foreign key (book_id) references book (id) on delete cascade
 );
 
 create table book_balance_changelog (
