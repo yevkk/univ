df594e68788f4e3f5dfd16e76e1b432c65c947bd
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2022-02-12 18:41:59 +0200

    add board templates

--- python-programming/peg_solitaire/board_template.py
+++ python-programming/peg_solitaire/board_template.py
@@ -19,4 +19,46 @@ class BoardTemplate:
         return self.__default_empty
 
 
+templates = {
+    'minimum': BoardTemplate(
+        (1, 3),
+        [(0, i) for i in range(3)],
+        (0, 0)
+    ),
 
+    'easy pinwheel': BoardTemplate(
+        (4, 4),
+        [(0, 2),
+         *[(1, i) for i in range(0, 3)],
+         *[(2, i) for i in range(1, 4)],
+         (2, 1)
+         ],
+        (1, 0)
+    ),
+
+    'english style': BoardTemplate(
+        (7, 7),
+        [*[(0, i) for i in range(2, 5)],
+         *[(1, i) for i in range(2, 5)],
+         *[(2, i) for i in range(0, 7)],
+         *[(3, i) for i in range(0, 7)],
+         *[(4, i) for i in range(0, 7)],
+         *[(5, i) for i in range(2, 5)],
+         *[(6, i) for i in range(2, 5)]
+         ],
+        (3, 3)
+    ),
+
+    'european style': BoardTemplate(
+        (7, 7),
+        [*[(0, i) for i in range(2, 5)],
+         *[(1, i) for i in range(1, 6)],
+         *[(2, i) for i in range(0, 7)],
+         *[(3, i) for i in range(0, 7)],
+         *[(4, i) for i in range(0, 7)],
+         *[(5, i) for i in range(1, 6)],
+         *[(6, i) for i in range(2, 5)]
+         ],
+        (3, 3)
+    )
+}
