69deb44935219f524cffc3f3dd5527e3ab52f2c5
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2022-02-13 01:28:17 +0200

    add board click response

--- python-programming/peg_solitaire/board_surface.py
+++ python-programming/peg_solitaire/board_surface.py
@@ -8,10 +8,13 @@ class BoardSurface(pygame.sprite.Sprite):
 
         self.__board = board
         self.__colors = colors
+
         self.__cell_radius = 0.9 * size / (2 * max(board.size))
         self.__cell_margin = 0.2 * self.__cell_radius
         self.__x_offset = (size - board.size[1] * 2 * self.__cell_radius - (board.size[1] - 1) * self.__cell_margin) / 2
         self.__y_offset = (size - board.size[0] * 2 * self.__cell_radius - (board.size[0] - 1) * self.__cell_margin) / 2
+
+        self.__selected = None
 
     def draw(self):
         self.surf.fill(self.__colors[0])
@@ -24,10 +27,35 @@ class BoardSurface(pygame.sprite.Sprite):
             if self.__board.cells[pos]:
                 pygame.draw.circle(self.surf, self.__colors[1], [center_x, center_y], 0.6 * self.__cell_radius)
 
+        if self.__selected:
+            pos = self.__selected
+            center_x = self.__x_offset + pos[1] * (2 * self.__cell_radius + self.__cell_margin) + self.__cell_radius
+            center_y = self.__y_offset + pos[0] * (2 * self.__cell_radius + self.__cell_margin) + self.__cell_radius
+            pygame.draw.circle(self.surf, self.__colors[3], [center_x, center_y], self.__cell_radius, 3)
+            pygame.draw.circle(self.surf, self.__colors[3], [center_x, center_y], 0.6 * self.__cell_radius)
+
     def mouse_highlight(self, mouse_pos):
-        print(mouse_pos)
         for pos in self.__board.cells:
             center_x = self.__x_offset + pos[1] * (2 * self.__cell_radius + self.__cell_margin) + self.__cell_radius
             center_y = self.__y_offset + pos[0] * (2 * self.__cell_radius + self.__cell_margin) + self.__cell_radius
             if (mouse_pos[0] - center_x) ** 2 + (mouse_pos[1] - center_y) ** 2 < self.__cell_radius ** 2:
                 pygame.draw.circle(self.surf, self.__colors[2], [center_x, center_y], self.__cell_radius, 3)
+
+    def mouse_click(self, mouse_pos):
+        clicked = None
+        for pos in self.__board.cells:
+            center_x = self.__x_offset + pos[1] * (2 * self.__cell_radius + self.__cell_margin) + self.__cell_radius
+            center_y = self.__y_offset + pos[0] * (2 * self.__cell_radius + self.__cell_margin) + self.__cell_radius
+            if (mouse_pos[0] - center_x) ** 2 + (mouse_pos[1] - center_y) ** 2 < self.__cell_radius ** 2:
+                clicked = pos
+                break
+
+        if not clicked:
+            return
+
+        if not self.__selected:
+            self.__selected = clicked
+        elif self.__selected == clicked:
+            self.__selected = None
+        elif self.__board.move(self.__selected, clicked):
+            self.__selected = None
--- python-programming/peg_solitaire/main.py
+++ python-programming/peg_solitaire/main.py
@@ -5,26 +5,33 @@ from board import Board
 
 BG_COLOR = (18, 18, 24)
 WHITE = (255, 255, 255)
-HIGHLIGHT_COLOR_1 = (255, 51, 0)
+HIGHLIGHT_COLOR_1 = (0, 215, 135)
+HIGHLIGHT_COLOR_2 = (255, 200, 0)
 
 BOARD_OFFSET = (100, 200)
+
+
+def board_mouse_pos(mouse_position):
+    return mouse_position[0] - BOARD_OFFSET[0], mouse_position[1] - BOARD_OFFSET[1]
+
 
 pygame.init()
 screen = pygame.display.set_mode((600, 800))
 screen.fill(BG_COLOR)
 
-board = BoardSurface(Board('english style'), 400, [BG_COLOR, WHITE, HIGHLIGHT_COLOR_1])
+board = BoardSurface(Board('english style'), 400, [BG_COLOR, WHITE, HIGHLIGHT_COLOR_1, HIGHLIGHT_COLOR_2])
 
 running = True
 while running:
     for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            board.mouse_click(board_mouse_pos(pygame.mouse.get_pos()))
+
         if event.type == QUIT:
             running = False
 
     board.draw()
+    board.mouse_highlight(board_mouse_pos(pygame.mouse.get_pos()))
 
-    mouse = pygame.mouse.get_pos()
-    board.mouse_highlight((mouse[0] - BOARD_OFFSET[0], mouse[1] - BOARD_OFFSET[1]))
-    
     screen.blit(board.surf, BOARD_OFFSET)
     pygame.display.flip()
