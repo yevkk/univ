20cb020ce8e87764a192a07c656e6f2c34f900d2
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2022-02-13 20:12:49 +0200

    add cython wrapper

---
+++ python-programming/cython_task/cython_wrapper.pyx
@@ -0,0 +1,18 @@
+from libc.stdlib cimport malloc
+
+cdef extern from "c_code.c":
+    char *f(int *keys, int *values, int size)
+
+def compute(input_data):
+    cdef int size = len(input_data)
+    cdef int *keys = <int *> malloc(size * sizeof(char))
+    cdef int *values = <int *> malloc(size * sizeof(char))
+
+    for i, key in enumerate(input_data):
+        keys[i] = key
+        values[i] = input_data[key]
+
+    cdef object cresult
+    cresult = f(keys, values, size)
+
+    return str(cresult)
