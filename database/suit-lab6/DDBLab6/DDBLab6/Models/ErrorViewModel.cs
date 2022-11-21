f5e8cbe6ec14c18c28326e27075a99254d49b686
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2022-11-22 00:07:51 +0200

    Project Initial Commit

---
+++ database/suit-lab6/DDBLab6/DDBLab6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DDBLab6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
