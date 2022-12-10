b77d49e5ab47f734bab6a9c8d2efc5d9b54cc205
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2022-12-10 02:41:17 +0200

    MVC components for employees

---
+++ database/suit-cw/DDB_CW/DDB_CW/Controllers/EmployeesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DDB_CW.Data;
+using DDB_CW.Models;
+
+namespace DDB_CW.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly DDB_CWContext _context;
+
+        public EmployeesController(DDB_CWContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Employees
+        public async Task<IActionResult> Index()
+        {
+              return _context.employees != null ? 
+                          View(await _context.employees.ToListAsync()) :
+                          Problem("Entity set 'DDB_CWContext.Employee'  is null.");
+        }
+
+        // GET: Employees/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.employees == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.employees
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Fullname,Email,Department,Position,HireYear")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(employee);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.employees == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Fullname,Email,Department,Position,HireYear")] Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employee);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeeExists(employee.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.employees == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.employees
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.employees == null)
+            {
+                return Problem("Entity set 'DDB_CWContext.Employee'  is null.");
+            }
+            var employee = await _context.employees.FindAsync(id);
+            if (employee != null)
+            {
+                _context.employees.Remove(employee);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeeExists(int id)
+        {
+          return (_context.employees?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- database/suit-cw/DDB_CW/DDB_CW/Data/DDB_CWContext.cs
+++ database/suit-cw/DDB_CW/DDB_CW/Data/DDB_CWContext.cs
@@ -17,5 +17,7 @@ namespace DDB_CW.Data
         public DbSet<DDB_CW.Models.Hall> halls { get; set; } = default!;
 
         public DbSet<DDB_CW.Models.Row> rows { get; set; } = default!;
+
+        public DbSet<DDB_CW.Models.Employee> employees { get; set; } = default!;
     }
 }
