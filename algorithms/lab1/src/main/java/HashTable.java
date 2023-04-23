6ea170ea12c5e8a7321afe88481bdc5445dd12db
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2023-04-24 00:21:01 +0300

    add hash table class

---
+++ algorithms/lab1/src/main/java/HashTable.java
@@ -0,0 +1,42 @@
+public class HashTable {
+    Square[] table;
+    int size;
+
+    public HashTable(int size) {
+        this.size = size;
+        this.table = new Square[size];
+    }
+
+    public boolean insert(Square square) {
+        var hash = (int) square.perimeter() % size;
+        if (table[hash] == null) {
+            table[hash] = square;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        var i_len_max = String.valueOf(size).length();
+
+        res.append("HASH TABLE\n").append("-".repeat(50)).append('\n');
+        for (int i = 0; i < size; i++) {
+            if (table[i] == null) continue;
+            var i_len = String.valueOf(i).length();
+            res.append(String.valueOf(i)).append(":").append(" ".repeat(i_len_max - i_len + 1));
+            res.append(table[i].toString()).append('\n');
+
+//            res.append(" ".repeat(i_len_max + 2));
+//            res.append("bbb\n");
+        }
+        res.append("-".repeat(50));
+
+        return res.toString();
+    }
+}
