6c9425bc1a4a1194f750e79b8fd9e6cf224b156d
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2023-04-30 17:11:30 +0300

    update hash table with lvl2 reqs (collisions resolution)

--- algorithms/lab1/src/main/java/HashTable.java
+++ algorithms/lab1/src/main/java/HashTable.java
@@ -1,20 +1,36 @@
+import java.util.LinkedList;
+
 public class HashTable {
     Square[] table;
+    LinkedList<Square>[] table_enhanced;
     int size;
+    boolean enhanced;
 
-    public HashTable(int size) {
+    public HashTable(int size, boolean enhanced) {
         this.size = size;
-        this.table = new Square[size];
+        this.enhanced = enhanced;
+        if (enhanced) {
+            this.table_enhanced = new LinkedList[size];
+            for (int i = 0; i < size; i++) {
+                this.table_enhanced[i] = new LinkedList<>();
+            }
+        } else {
+            this.table = new Square[size];
+        }
     }
 
     public boolean insert(Square square) {
         var hash = (int) square.perimeter() % size;
-        if (table[hash] == null) {
-            table[hash] = square;
-            return true;
+
+        if (enhanced) {
+            table_enhanced[hash].addLast(square);
         } else {
-            return false;
+            if (table[hash] != null) {
+                return false;
+            }
+            table[hash] = square;
         }
+        return true;
     }
 
     public int size() {
@@ -25,15 +41,21 @@ public class HashTable {
         StringBuilder res = new StringBuilder();
         var i_len_max = String.valueOf(size).length();
 
-        res.append(String.format("HASH TABLE (%d)\n", size())).append("-".repeat(50)).append('\n');
+        res.append(String.format("HASH TABLE (s: %d, e: %s)\n", size(), String.valueOf(enhanced))).append("-".repeat(50)).append('\n');
         for (int i = 0; i < size; i++) {
-            if (table[i] == null) continue;
+            if ((enhanced && table_enhanced[i].isEmpty()) || (!enhanced && table[i] == null)) continue;
             var i_len = String.valueOf(i).length();
             res.append(String.valueOf(i)).append(":").append(" ".repeat(i_len_max - i_len + 1));
-            res.append(table[i].toString()).append('\n');
 
-//            res.append(" ".repeat(i_len_max + 2));
-//            res.append("bbb\n");
+            if (enhanced) {
+                res.append(table_enhanced[i].get(0).toString()).append('\n');
+                for (int j = 1; j < table_enhanced[i].size(); j++) {
+                    res.append(" ".repeat(i_len_max + 2));
+                    res.append(table_enhanced[i].get(j).toString()).append('\n');
+                }
+            } else {
+                res.append(table[i].toString()).append('\n');
+            }
         }
         res.append("-".repeat(50));
 
