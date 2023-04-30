70b6bb0c69f61a7fedb12a37b7767c9569c5837e
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2023-05-01 00:45:49 +0300

    add quick sort

--- algorithms/lab2/src/main/java/Sort.java
+++ algorithms/lab2/src/main/java/Sort.java
@@ -1,16 +1,24 @@
 import java.util.LinkedList;
 
 public class Sort {
-    public static void bubbleSort(Student[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
+    private static void swap(Student[] arr, int first, int second) {
+        var tmp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = tmp;
+    }
+
+    private static void bubbleSortInternal(Student[] arr, int first, int last) {
+        for (int i = first; i < last - 1; i++) {
+            for (int j = first; j < last - i - 1; j++) {
                 if (arr[j].getGroupNo() > arr[j + 1].getGroupNo()) {
-                    var tmp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = tmp;
+                    swap(arr, j, j + 1);
                 }
             }
         }
+    }
+
+    public static void bubbleSort(Student[] arr) {
+        bubbleSortInternal(arr, 0, arr.length);
     }
 
     public static void indexSort(Student[] arr) {
@@ -37,11 +45,62 @@ public class Sort {
         }
 
         int index = 0;
-        for (int i = 0; i < lists.length; i++) {
-            for (var student : lists[i]) {
+        for (var list : lists) {
+            for (var student : list) {
                 arr[index] = student;
                 index++;
             }
         }
+    }
+
+    private static int quickSortPartition(Student[] arr, int first, int last) {
+        int middle = (first + last) / 2;
+
+        //sorting first, last and middle
+        if (arr[first].getGroupNo() > arr[middle].getGroupNo()) {
+            swap(arr, first, middle);
+        }
+        if (arr[middle].getGroupNo() > arr[last].getGroupNo()) {
+            swap(arr, middle, last);
+        }
+        if (arr[first].getGroupNo() > arr[middle].getGroupNo()) {
+            swap(arr, first, middle);
+        }
+
+        if (last - first <= 2) return middle;
+
+        swap(arr, middle, last - 1);
+        var pivot = last - 1;
+        int i = first;
+        int j = last - 1;
+
+        while (i < j) {
+            while(arr[i].getGroupNo() < arr[pivot].getGroupNo()) {
+                i++;
+                if (i == last) break;
+            }
+            while(arr[j].getGroupNo() >= arr[pivot].getGroupNo()) {
+                j--;
+                if (j == 0) break;
+            }
+            if (i < j) {
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, i, pivot);
+        return i;
+    }
+
+    private static void quickSortInternal(Student[] arr, int first, int last) {
+        if (first < last) {
+            int div = quickSortPartition(arr, first, last);
+            quickSortInternal(arr, first, div - 1);
+            quickSortInternal(arr, div + 1, last);
+        }
+    }
+
+    public static void quickSort(Student[] arr) {
+        quickSortInternal(arr, 0, arr.length - 1);
     }
 }
