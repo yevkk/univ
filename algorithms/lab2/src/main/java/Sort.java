cefc05915412c7e8b704fc3ccb405c538a8405f0
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2023-04-30 21:30:19 +0300

    add index sort

--- algorithms/lab2/src/main/java/Sort.java
+++ algorithms/lab2/src/main/java/Sort.java
@@ -12,4 +12,36 @@ public class Sort {
             }
         }
     }
+
+    public static void indexSort(Student[] arr) {
+        int size = 0;
+        for (var student : arr) {
+            if (student.getGroupNo() > size) {
+                size = student.getGroupNo();
+            }
+        }
+
+        LinkedList<Student>[] lists = new LinkedList[size + 1];
+        for (int i = 0; i < lists.length; i++) {
+            lists[i] = new LinkedList<Student>();
+        }
+
+        for (var student : arr) {
+            int i;
+            for (i = 0; i < lists[student.getGroupNo()].size(); i++) {
+                if (lists[student.getGroupNo()].get(i).getStudentCardNo() >= student.getStudentCardNo()) {
+                    break;
+                }
+            }
+            lists[student.getGroupNo()].add(i, student);
+        }
+
+        int index = 0;
+        for (int i = 0; i < lists.length; i++) {
+            for (var student : lists[i]) {
+                arr[index] = student;
+                index++;
+            }
+        }
+    }
 }
