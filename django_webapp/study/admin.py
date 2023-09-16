a21dc2c8099936070b2296d8e05ea3c0f479911f
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2023-09-17 01:27:18 +0300

    added admin models

--- django_webapp/study/admin.py
+++ django_webapp/study/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from nested_inline.admin import NestedStackedInline, NestedModelAdmin
+
+from .models import *
+
+
+class CourseAdmin(admin.ModelAdmin):
+    model = Course
+    list_display  = ['course_name', 'course_code']
+    search_fields = ['course_name']
+
+
+class LectureAdmin(admin.ModelAdmin):
+    model = Lecture
+    list_display  = ['lecture_name', 'lecture_number', 'course']
+    list_filter   = ['course']
+
+
+class OptionInline(NestedStackedInline):
+    model = Option
+    extra = 1
+
+
+class TaskInline(NestedStackedInline):
+    model   = Task
+    extra   = 1 
+    inlines = [OptionInline]
+
+
+class TestAdmin(NestedModelAdmin):
+    fields = [
+        'course',
+        'test_number',
+        'test_name',
+        'pass_score',
+        'enabled',
+        ]
+
+    inlines       = [TaskInline]
+    list_display  = ['test_name', 'test_number', 'course']
+    list_filter   = ['course']
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lecture, LectureAdmin)
+admin.site.register(Test, TestAdmin)
+
+admin.site.unregister(Group)
