ef8f75ae71009365868e30ae88a4df1ad5052cd5
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2023-09-18 01:07:43 +0300

    added index, course and lecture views with draft templates

--- django_webapp/study/urls.py
+++ django_webapp/study/urls.py
@@ -7,6 +7,8 @@ app_name    = 'study'
 urlpatterns = [
     path('login/',  auth_views.LoginView.as_view(), name='login'),
     path('logout/', views.logout_view,              name='logout'),
-    
-    path('', views.index, name='index'),
+
+    path('',                          views.index,   name='index'),
+    path('course/<int:course_id>/',   views.course,  name='course'),
+    path('lecture/<int:lecture_id>/', views.lecture, name='lecture')
 ]
--- django_webapp/study/views.py
+++ django_webapp/study/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import logout
@@ -14,11 +14,48 @@ def logout_view(request):
 
 @login_required(login_url="login/")
 def index(request):
-    course_list = Course.objects.all()
+    all_courses       = Course.objects.all()
+    completed_courses = CompletedCourses.objects.filter(user=request.user.id)
+
+    course_list = [{
+        'e'         : course,
+        'completed' : (course in completed_courses),
+        'available' : all([prec in completed_courses for prec in course.preconditions.all()])
+        } for course in all_courses]
     
     context = {
-        'user': request.user,
-        'course_list': course_list
+        'user'        : request.user,
+        'course_list' : course_list
     }
     return render(request, 'study/index.html', context)
+
+
+@login_required(login_url="login/")
+def course(request, course_id):
+    course       = get_object_or_404(Course, pk=course_id)
+    course_tests = Test.objects.filter(course=course_id)
+
+    test_list = [{
+        'e'      : test,
+        'passed' : any([t_res.score >= test.pass_score for t_res in TestResult.objects.filter(test=test.id)]),
+        'failed' : any([t_res.score <  test.pass_score for t_res in TestResult.objects.filter(test=test.id)])
+        } for test in course_tests]
+    
+    context = {
+        'user'      : request.user,
+        'course'    : course,
+        'test_list' : test_list
+    }
+    return render(request, 'study/course.html', context)
+
+
+@login_required(login_url="login/")
+def lecture(request, lecture_id):
+    lecture = get_object_or_404(Lecture, pk=lecture_id)
+    
+    context = {
+        'user'    : request.user,
+        'lecture' : lecture,
+    }
+    return render(request, 'study/lecture.html', context)
 
