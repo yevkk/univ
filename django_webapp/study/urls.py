5d0465c9d3be44910859223e17c599f83064636e
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2023-09-17 22:47:05 +0300

    added logic/logout logic

---
+++ django_webapp/study/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from . import views
+
+app_name    = 'study'
+urlpatterns = [
+    path('login/',  auth_views.LoginView.as_view(), name='login'),
+    path('logout/', views.logout_view,              name='logout'),
+    
+    path('', views.index, name='index'),
+]
--- django_webapp/study/views.py
+++ django_webapp/study/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import *
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('study:index'))
+
+
+@login_required(login_url="login/")
+def index(request):
+    course_list = Course.objects.all()
+    
+    context = {
+        'user': request.user,
+        'course_list': course_list
+    }
+    return render(request, 'study/index.html', context)
+
