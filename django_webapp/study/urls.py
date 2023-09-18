b7e691e983a415ba6b9138f8dfc1f9749284e091
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2023-09-19 00:32:58 +0300

    added test logic and view

--- django_webapp/study/urls.py
+++ django_webapp/study/urls.py
@@ -8,8 +8,10 @@ urlpatterns = [
     path('login/',  auth_views.LoginView.as_view(), name='login'),
     path('logout/', views.logout_view,              name='logout'),
 
-    path('',                          views.index,        name='index'),
-    path('course/<int:course_id>/',   views.course,       name='course'),
-    path('lecture/<int:lecture_id>/', views.lecture,      name='lecture'),
-    path('account_info/',             views.account_info, name='account_info')
+    path('',                           views.index,        name='index'),
+    path('course/<int:course_id>/',    views.course,       name='course'),
+    path('lecture/<int:lecture_id>/',  views.lecture,      name='lecture'),
+    path('test/<int:test_id>/',        views.test,         name='test'),
+    path('submit_test/<int:test_id>/', views.submit_test,  name='submit_test'),
+    path('account_info/',              views.account_info, name='account_info')
 ]
--- django_webapp/study/views.py
+++ django_webapp/study/views.py
@@ -3,6 +3,9 @@ from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.utils import timezone
+from django.db.models import F
 
 from .models import *
 
@@ -58,6 +61,58 @@ def lecture(request, lecture_id):
         'lecture' : lecture,
     }
     return render(request, 'study/lecture.html', context)
+
+
+@login_required(login_url="login/")
+def test(request, test_id):
+    test    = get_object_or_404(Test, pk=test_id)
+    results = TestResult.objects.filter(test=test.id, user=request.user.id)
+    
+    context = {
+        'user'    : request.user,
+        'test'    : test,
+        'results' : results.order_by('-dt')
+    }
+    return render(request, 'study/test.html', context)
+
+
+@login_required(login_url="login/")
+def submit_test(request, test_id):
+    test      = get_object_or_404(Test, pk=test_id)
+    tasks     = test.task_set.all()
+    max_score = len(tasks)
+    score     = 0
+
+    for task in tasks:
+        options = task.option_set.all()
+        
+        false_opt_ids, true_opt_ids = [], []
+        for opt in options:
+            (false_opt_ids, true_opt_ids)[opt.correct].append(opt.id)
+        
+        if not any([f'opt-{id}' in request.POST and request.POST[f'opt-{id}'] == 'on' for id in false_opt_ids]) \
+           and all([f'opt-{id}' in request.POST and request.POST[f'opt-{id}'] == 'on' for id in true_opt_ids]):
+            score += 1
+    
+    score_percent = score / max_score * 100
+    
+    # Save result
+    res = TestResult(user=request.user, test=test, score=score_percent, dt=timezone.now())
+    res.save()
+
+    # Update stats
+    if score_percent >= test.pass_score:
+        test.stats_passed = F('stats_passed') + 1
+    else:
+        test.stats_failed = F('stats_failed') + 1
+
+    # Complete course
+    course_tests = Test.objects.filter(course=test.course.id)
+    if all([TestResult.objects.filter(user=request.user, test=t, score__gte=t.pass_score).exists() for t in course_tests]):
+        c = CompletedCourses(user=request.user, course=test.course)
+        c.save()
+
+    return HttpResponseRedirect(reverse('study:course', args=(test.course.id, )))
 
 
 @login_required(login_url="login/")
